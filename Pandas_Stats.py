b8c52cb9030e72625093f77652bc3f30d88e8d71
B Seamus Haney <43055154+Bhaney44@users.noreply.github.com>
2020-06-22 03:17:41 -0400

    Add files via upload

--- Pandas_Stats.py
+++ Pandas_Stats.py
@@ -51,35 +51,6 @@ print(df.cov())
 #print(df.mod())
 
 
-#print(df.columns)
-#print(df.columns[0])
-#print(df.columns[1])
-#print(df.columns[2])
-
-claims = df.columns[0]
-prior_art = df.columns[1]
-inventors = df.columns[2]
-
-#for index, row in df.iterrows():
-    #print(claims, prior_art, inventors)
-
-    
-
-#with open('Total_Python.csv','r') as csvfile:
-   # plots = csv.reader(csvfile, delimiter=',')
-   # for row in plots:
-       # x.append(int(row[0]))
-       # y.append(int(row[1]))
-       # z.append(int(row[2]))
 
 
-#threedee = plt.figure().gca(projection='3d')
-#threedee.scatter(claims, prior_art, inventors)
-#threedee.set_xlabel('Claims')
-#threedee.set_ylabel('Cited Proior Art')
-#threedee.set_zlabel('Inventors')
 
-
-#plt.title('Thyroid Patents')
-#plt.legend()
-#plt.show()
